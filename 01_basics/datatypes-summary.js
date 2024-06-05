e0ece4aa7ace79618622875ccaa2b8933f879865
Ansil Shah <40911569+Ansil09@users.noreply.github.com>
2024-06-05 19:44:49 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, Bigint
+
+// Data Types
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = nulll
+let userEmail;             => undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 6327568237658n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "hanuman"];
+let myObj = {
+    name: "ansil",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof scoreValue);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
