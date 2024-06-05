c698ad319021c1de573a08d32b9b9a0e5b4147a4
Ansil Shah <40911569+Ansil09@users.noreply.github.com>
2024-06-05 18:31:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33absc" => NaN
+// true => 1; false => 0;
+// "Ansil" => true
+
+// let isLoggedIn = 1
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// let someNumber = 33
+
+// let stringNumber= string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let isLoggedIn = "Ansil"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
