0ad29593f488383a453e738d5f4537c8282cf60e
Ansil Shah <40911569+Ansil09@users.noreply.github.com>
2024-06-05 17:38:26 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use srict"; // treat all JS code as newer version
+
+// alert(3 + 3)         // we are using node.js not browser
+
+console.log(3 + 
+        
+    3) // code readability should be high
+
+console.log("Ansil")
+
+let name = "Ansil"
+let age = 25
+let isLoggedIn = false
+let state;
+
+//  number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true/false
+//  null => standalone value 
+//  undefined => 
+//  symbol => unique
+//  object
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
