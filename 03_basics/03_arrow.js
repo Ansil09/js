a1cbc2d7148b7a0044ae33b7db82275269e19b11
Ansil Shah <40911569+Ansil09@users.noreply.github.com>
2024-06-08 05:21:53 +0000

    arrows

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "ansil",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} ,  welcome to website`);
+        console.log(this);
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+
+// console.log(this);
+
+
+
+// function chai(){
+//     let username = "ansilk"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+
+
+// const chai = () => {
+//     let username = "anilk"
+//     console.log(this);
+// }
+
+// chai()
+
+
+ 
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+//  console.log(addTwo(3,4));
+
+//  const addTwo =  (num1, num2) => (num1 + num2)
+//  console.log(addTwo(3,3));
+
+//  const addTwo = (num1, num2) => ({username: "ansil"})
+//  console.log(addTwo(3, 4));
+
